df1f43ce866c8e802cfd8381acd2162a2b134a3f
EHLI <jennykim@cmu-809193.wv.cc.cmu.edu>
2012-06-21 12:11:34 -0400

    Initial Commit

---
+++ Puzzle2/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Puzzle2
+//
+//  Created by Jenny Kim on 6/21/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
